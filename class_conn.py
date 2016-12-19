3d7635a235991976a4fb0feba84a01c31e07ac54
Jason Ashton <jlashton@wpi.edu>
2016-12-19 15:19:30 -0500

    Preliminarily working with Amazon's Alexa Service. Only supports getting grades of recent assignments ATM

---
+++ class_conn.py
@@ -0,0 +1,71 @@
+import canvaslms.api as api
+import json
+from datetime import datetime
+
+
+class canvasConn:
+
+    def __init__(self, tokenLocation='auth-token.txt', server='canvas.wpi.edu'):
+        self.tokenLocation = tokenLocation
+        self.server = server
+
+        self.conn = api.CanvasAPI(server, api.getAuthTokenFromFile(tokenLocation))
+
+
+    '''
+        Returns IDs of classes in list
+    '''
+    def list_classes_id(self):
+        classes = []
+        results = self.conn.allPages('courses')
+
+        for i in range(len(results)):
+            if 'name' in results[i]:
+                classes.append(results[i]['id'])
+
+        return(classes)
+
+
+    '''
+    Returns ID of last updated assignment
+    '''
+    def get_last_asst(self, class_id):
+        results = self.conn.allPages('courses/{}/assignments'.format(class_id))
+
+        recent_index = 0
+        recent_time = datetime.strptime(results[0]['updated_at'], '%Y-%m-%dT%H:%M:%SZ')
+
+        for i in range(len(results)):
+            time = datetime.strptime(results[i]['updated_at'], '%Y-%m-%dT%H:%M:%SZ')
+
+            if time > recent_time:
+                recent_time = time
+                recent_index = i
+
+        return int(results[recent_index]['id'])
+
+
+    '''
+        Get last grade for an assignment
+    '''
+    def get_asst_grade(self, class_id, asst_id):
+        results = self.conn.allPages('courses/{}/assignments/{}/submissions/self'.format(class_id, asst_id))
+        score = results[-1]['score']
+        if score is not None:
+            return float(score)
+
+    def print_last_submission(self, class_id):
+        course = self.conn.allPages('courses/{}'.format(class_id))[0]['name']
+
+        asst_id = self.get_last_asst(class_id)
+
+        asst = self.conn.allPages('courses/{}/assignments/{}'.format(class_id, asst_id))[0]
+
+        asst_name = asst['name']
+        asst_total = asst['points_possible']
+
+        grade = self.get_asst_grade(class_id, asst_id)
+
+        return('<p>{}:<break strength="strong" /> Last Assignment Updated: {} <break strength="strong" />Grade: <say-as interpret-as="unit">{}</say-as><break strength="weak" /> out of <say-as interpret-as="unit">{}</say-as></p>'.format(course, asst_name, grade, asst_total))
+
+
--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import class_conn
-if __name__ == "__main__":
-    #Algo: 1843
-    #Exam2: 2421
+from flask import Flask, render_template
+from flask_ask import Ask, statement, question, session
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+@ask.launch
+def launched():
+    return question('Welcome to Jason\'s Canvas app. Say: get my recent grades or quit')
+
+@ask.intent("RecentGradesIntent")
+def get_recent_grades():
 
     classes = class_conn.canvasConn()
+    classes_statement = '<speak> '
+
     for i in classes.list_classes_id():
-        classes.print_last_submission(i)
+        classes_statement += classes.print_last_submission(i)
+
+    classes_statement += ' </speak>'
+
+    return statement(classes_statement)
 
 
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
